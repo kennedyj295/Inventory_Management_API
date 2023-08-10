625813d8fc0bc4cf1493cc2fba831b4b975b3992
kennedyj295 <115056868+kennedyj295@users.noreply.github.com>
2023-08-09 22:55:18 -0500

    added db context

---
+++ MyDBContext.cs
@@ -0,0 +1,12 @@
+﻿using InventoryManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
